5a9c9aed9891f0aee6a82cb86d4c739ed561d98c
violettomania <danielpolya@protonmail.com>
2023-11-15 15:29:02 +0100

    feature: project scaffolding , styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,44 @@
 export default function App() {
   return (
-    <main>
-      <div>
-        <p>it works!</p>
+    <section className='section-center'>
+      <div className='Toastify'></div>
+      <form>
+        <h4>grocery bud</h4>
+        <div className='form-control'>
+          <input
+            type='text '
+            className='form-input'
+            value=''
+            onChange={() => {}}
+          />
+          <button type='submit' className='btn'>
+            add item
+          </button>
+        </div>
+      </form>
+      <div className='items'>
+        <div className='single-item'>
+          <input type='checkbox' onChange={() => {}} />
+          <p style={{ textTransform: 'capitalize' }}>stuff</p>
+          <button className='btn remove-btn' type='button'>
+            delete
+          </button>
+        </div>
+        <div className='single-item'>
+          <input type='checkbox' onChange={() => {}} />
+          <p
+            style={{
+              textTransform: 'capitalize',
+              textDecoration: 'line-through',
+            }}
+          >
+            new stuff
+          </p>
+          <button className='btn remove-btn' type='button'>
+            delete
+          </button>
+        </div>
       </div>
-    </main>
+    </section>
   );
 }
